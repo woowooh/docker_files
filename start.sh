21479d838f1e3f217b44db5154be054222263c4b
eva <>
2019-12-22 22:32:08 +0800

    1. 修改 dockerfile 2. 简单的启动脚本

--- start.sh
+++ start.sh
@@ -1,5 +1,8 @@
 docker build -t xf/ubuntu:18.04 .
-docker run -d -p 8080:8080 xf/ubuntu:18.04
+docker run --name="test" -d -p 8080:8080 xf/ubuntu:18.04
 
+docker exec -it test /bin/bash
+# docker rm $(docker ps -a -q)
+# docker cp /Users/eva/Documents/repository/com/study/omg/1.0-SNAPSHOT/omg-1.0-SNAPSHOT.war test:/opt/soft/
 # 删除 docker rmi -f xf/ubuntu:18.04
 # docker stop xf/ubuntu:18.04
